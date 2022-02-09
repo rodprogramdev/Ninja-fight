d3b0fd40f5a94666ca11c8fec1fe605884d5a2d6
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-02-09 13:48:55 +0800

    update player.x and player.y of player object to reposition character

--- scripts/Main.js
+++ scripts/Main.js
@@ -6,8 +6,8 @@ canvas.height = 500;
 const keys = [];
 
 const player = {
-    x: 0, // x axis
-    y: 0, // y axis
+    x: 100, // x axis
+    y: 380, // y axis
     width: 520,
     height:463,
     frameX: 0, // will store coordinates rectangle of frame of the image, spritesheet is compose of images
@@ -35,11 +35,21 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
 function animate(){
     canvasContext.clearRect(0,0,canvas.width, canvas.height);
     canvasContext.drawImage(background,0,0, canvas.width, canvas.height); // built in canvas method three versions.
-    drawSprite(playerSprite, 0, 0, player.width, player.height, 0,0, player.width /6,player.height/6);
+    drawSprite(playerSprite, 0, 0, player.width, player.height, player.x,player.y, player.width /6,player.height/6);
     requestAnimationFrame(animate);
 }
 
 animate();
+
+
+window.addEventListener("keydown",function(e){
+     keys[e.key] = true;// attribute // callback function (e) has a built in object
+     console.log(keys);
+});
+
+window.addEventListener("keyup", function(e){
+    delete keys[e.key];
+});
 
 
 
