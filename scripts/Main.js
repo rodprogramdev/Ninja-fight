e1f405d037101274fa845b9b909e8e0cd4e8d35d
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-02-09 13:58:57 +0800

    player.frameX & player.frameY working

--- scripts/Main.js
+++ scripts/Main.js
@@ -10,7 +10,7 @@ const player = {
     y: 380, // y axis
     width: 520,
     height:463,
-    frameX: 0, // will store coordinates rectangle of frame of the image, spritesheet is compose of images
+    frameX: 2, // will store coordinates rectangle of frame of the image, spritesheet is compose of images
     frameY: 0,
     speed: 9, // how many pixel per frame.
     moving: false
@@ -35,21 +35,27 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
 function animate(){
     canvasContext.clearRect(0,0,canvas.width, canvas.height);
     canvasContext.drawImage(background,0,0, canvas.width, canvas.height); // built in canvas method three versions.
-    drawSprite(playerSprite, 0, 0, player.width, player.height, player.x,player.y, player.width /6,player.height/6);
+    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x,player.y, player.width /6,player.height/6);
     requestAnimationFrame(animate);
+    movePlayer();
 }
 
 animate();
 
 
 window.addEventListener("keydown",function(e){
-     keys[e.key] = true;// attribute // callback function (e) has a built in object
+     keys[e.keyCode] = true;// attribute // callback function (e) has a built in object
      console.log(keys);
 });
 
 window.addEventListener("keyup", function(e){
-    delete keys[e.key];
+    delete keys[e.keyCode];
 });
 
+function movePlayer(){
+    if (keys[38] && player.y > 100){
 
+        player.y -= player.speed;
+    }
+}
 
